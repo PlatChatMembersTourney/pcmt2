f94534ba9bfb31cf3562d8a0c36a19d5fef3f051
sting <stinggyray@gmail.com>
2026-06-26 01:43:31 -0500

    forgot light mode existed

--- src/components/Teams.tsx
+++ src/components/Teams.tsx
@@ -16,7 +16,7 @@ const Teams: React.FC<TeamsProps> = (props: TeamsProps) => {
 		<div className="grid grid-cols-2 md:grid-cols-4 gap-4">
 			{
 				sortedTeams.map(({ name, abbr, logo, players }) => (
-					<div className="bg-neutral-800 h-40 flex flex-col items-center justify-center my-auto rounded-lg">
+					<div className="bg-neutral-200 dark:bg-neutral-800 h-40 flex flex-col items-center justify-center my-auto rounded-lg">
 						<img src={logo} className="w-20 h-20" />
 						<h1>{name}</h1>
 					</div>
